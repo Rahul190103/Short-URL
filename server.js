091ef99fd4b21f9202ae2ec7593b781f7855888c
Rahul190103 <rahulchhangani0@gmail.com>
2023-06-23 22:48:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
 // console.log(process.env.MONGO_URI);
 
-mongoose.connect( process.env.MONGO_URI , {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
+try {
+  mongoose.connect( process.env.MONGO_URI , {
+    useNewUrlParser: true, useUnifiedTopology: true
+  
+  })
+} catch (error) {
+  console.log(error);
+}
+
+
 
 app.set('view engine' , 'ejs')
 app.use(express.urlencoded({ extended: false }))
